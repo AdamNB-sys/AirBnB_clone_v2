2bd84416197f98019e0afb43f8169fbee1d53072
AdamNB-sys <a1adnat@gmail.com>
2022-03-18 15:43:24 -0500

    task 1, display HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#########################################
+# This script starts a flask web app    #
+# listening on 0.0.0.0, port 5000       #
+# routes: /: display "Hello HBNB"       #
+# with the option strict_slashes=False  #
+#########################################
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """uses Flask web app to display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """uses Flask web app to display 'HBNB'"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """start the web app as a script"""
+    app.run(host="0.0.0.0", port=5000)
